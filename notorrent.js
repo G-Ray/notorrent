348100a2268b3e47ea3ad651085f25d0730bb87c
G-Ray <geoffrey.bonneville@gmail.com>
2017-12-22 21:19:20 +0100

    Use memory-chunk-store

--- notorrent.js
+++ notorrent.js
@@ -1,5 +1,6 @@
 const { shell, Notification } = require('electron')
 const WebTorrent = require('webtorrent')
+const mem = require('memory-chunk-store')
 
 const Server = require('./lib/server')
 
@@ -22,14 +23,14 @@ class NoTorrent {
   }
 
   addTorrent (magnet) {
-    client.add(magnet, (torrent) => {
+    client.add(magnet, { store: mem }, (torrent) => {
       console.log('addTorrent')
       this._torrents.push(torrent)
       let server = new Server(torrent)
       server.listen()
       let address = server.address()
       this._showNotification(torrent)
-      // shell.openExternal('http://localhost:' + address.port + '/0')
+      shell.openExternal('http://localhost:' + address.port + '/0')
     })
   }
 }
