12ddfe96a181ed7c11f77afacab69065913b1149
G-Ray <geoffrey.bonneville@gmail.com>
2017-12-23 14:08:12 +0100

    Timeout after 10 seconds

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const NoTorrent = require('./notorrent')
 const noTorrent = new NoTorrent()
 
 const isSecondInstance = app.makeSingleInstance((argv, workingDirectory) => {
-  console.log(argv)
   noTorrent.addTorrent(argv[2])
 })
 
--- notorrent.js
+++ notorrent.js
@@ -11,29 +11,35 @@ class NoTorrent {
     this._torrents = []
   }
 
-  _showNotification (torrent) {
+  _showNotification (msg) {
     let notif = new Notification({
       title: 'NoTorrent',
-      body: torrent.name + ' is downloading'
+      body: msg
     })
-
-    notif.on('show', () => console.log('show'))
     notif.show()
   }
 
+  _onTimeout (torrent) {
+    console.log('timeOut')
+    torrent.destroy()
+    this._showNotification('This torrent has not enough seeders, please try another one')
+  }
+
   addTorrent (magnet) {
-    client.add(magnet, { store: mem }, (torrent) => {
-      console.log('addTorrent')
-      // torrent.files.forEach(f => f.deselect())
+    console.log('addTorrent')
+    let torrent = client.add(magnet, { store: mem }, (torrent) => {
+      clearTimeout(timeout)
       this._torrents.push(torrent)
       let server = new Server(torrent)
       server.listen()
       let address = server.address()
-      this._showNotification(torrent)
+      this._showNotification(torrent.name + ' is downloading')
       let length = torrent.files.length
       let index = length > 1 ? length : 0
       shell.openExternal('http://localhost:' + address.port + '/' + index)
     })
+
+    let timeout = setTimeout(() => this._onTimeout(torrent), 10000)
   }
 }
 
