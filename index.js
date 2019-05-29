77221996808592590b7b7fc6996a4c38ab901df3
G-Ray <geoffrey.bonneville@gmail.com>
2019-05-30 00:48:58 +0200

    Upgrade deps

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const utils = require('./lib/utils')
 
 const noTorrent = new NoTorrent()
 
-const isSecondInstance = app.makeSingleInstance((argv, workingDirectory) => {
-  let args = utils.sliceArgv(argv)
+const lock = app.requestSingleInstanceLock()
+
+if (!lock) {
+  return app.quit()
+}
+
+app.on('second-instance', (event, argv, workingDirectory) => {
+  const args = utils.sliceArgv(argv)
   noTorrent.addTorrent(args[0])
 })
 
-if (isSecondInstance) {
-  app.quit()
-}
-
-let args = utils.sliceArgv(process.argv)
+const args = utils.sliceArgv(process.argv)
 noTorrent.addTorrent(args[0])
